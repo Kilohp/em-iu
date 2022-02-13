b9a5b8de1747b4d7ba363b90af52175abc58d0e0
Kilohp <99595423+Kilohp@users.noreply.github.com>
2022-02-13 19:46:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,13 @@
 const CONFIG = {
     introTitle: 'Mr Bean à!',
-    introDesc: `Dans le royaume de mon coeur, cest toi la princesse`,
+    introDesc: `Dans le royaume de mon coeur
+    Cest toi la princesse`,
     btnIntro: 'hihi',
     title: 'On sest déjà vu quelque part, non?',
     desc: 'Tu ressembles énormément à ma prochaine petite copine ',
     btnYes: 'Em cũng rất thích anh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 8 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
