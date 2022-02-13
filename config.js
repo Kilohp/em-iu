76bd92b871776815e477a8b4cca8e1eb1f9dcc8f
Kilohp <99595423+Kilohp@users.noreply.github.com>
2022-02-13 19:34:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Mr Bean à!',
+    introDesc: `Dans le royaume de mon coeur, c'est toi la princesse`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'On s'est déjà vu quelque part, non?',
+    desc: 'Tu ressembles énormément à ma prochaine petite copine ',
+    btnYes: 'Em cũng rất thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
